b106dd414400d0b8c3f42490f84ad31e16c2d080
Julia <yuliya.sandu26@gmail.com>
2026-04-27 10:38:04 +0200

    Update Program2Window.hpp

--- Program2Window.hpp
+++ Program2Window.hpp
@@ -1,1 +1,34 @@
+#ifndef PROGRAM2WINDOW_HPP
+#define PROGRAM2WINDOW_HPP
 
+#include <QMainWindow>
+#include <QTableWidget>
+#include <QPushButton>
+#include <QLabel>
+
+class Program2Window : public QMainWindow
+{
+    Q_OBJECT
+public:
+    explicit Program2Window(QWidget *parent = nullptr);
+
+private slots:
+    void onSelectFile();
+    void onMoveBrokenToFile();
+
+private:
+    void loadFromFile(const QString &fileName);
+    void updateTables();
+
+    QPushButton *m_selectBtn;
+    QPushButton *m_moveBtn;
+    QLabel      *m_fileLabel;
+    QTableWidget *m_tableValid;
+    QTableWidget *m_tableBroken;
+
+    QString m_currentFilePath;
+    QList<InkObject> m_validObjects;
+    QList<InkObject> m_brokenObjects;
+};
+
+#endif // PROGRAM2WINDOW_HPP
