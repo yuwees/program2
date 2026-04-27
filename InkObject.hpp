e549c893397c95a354489de44ef11ee227c20e63
Julia <yuliya.sandu26@gmail.com>
2026-04-27 10:38:29 +0200

    Update InkObject.hpp

--- InkObject.hpp
+++ InkObject.hpp
@@ -1,1 +1,36 @@
+#ifndef INKOBJECT_HPP
+#define INKOBJECT_HPP
 
+#include <QString>
+#include <QJsonObject>
+
+class InkObject
+{
+public:
+    InkObject();
+    InkObject(const QString &name, const QString &desc, const QString &color, int inkLevel);
+
+    bool isValid() const;
+    bool parseFromTxtLine(const QString &line);
+
+    QJsonObject toJson() const;
+    void fromJson(const QJsonObject &obj);
+
+    QString getName() const;
+    QString getDescription() const;
+    QString getColor() const;
+    int getInkLevel() const;
+
+    void setName(const QString &name);
+    void setDescription(const QString &desc);
+    void setColor(const QString &color);
+    void setInkLevel(int level);
+
+private:
+    QString m_name;
+    QString m_description;
+    QString m_color;
+    int m_inkLevel;
+};
+
+#endif // INKOBJECT_HPP
