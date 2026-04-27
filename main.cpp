1130104a01ec06e4633a19fadb53bfd802e93641
Julia <yuliya.sandu26@gmail.com>
2026-04-27 10:37:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,10 @@
+#include <QApplication>
+#include "Program2Window.hpp"
 
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    Program2Window w;
+    w.show();
+    return app.exec();
+}
